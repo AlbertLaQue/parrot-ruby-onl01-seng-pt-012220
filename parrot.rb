900ecce56afcfb246609d590d4acd90ad687529f
Albert LaQue <albertlaque@gmail.com>
2020-01-28 03:28:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,3 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-#should accept an argument 
+#should accept an argument of a String
+#should puts out the string and return 
+#default argument of "Squawk!"
+#the return value of puts will always be nil 
+
+def parrot()
